35e6e26d83b80444c211bf1492d9fa5b4986382c
Stefan Jesner <stjesner@gmail.com>
2023-07-16 21:25:58 +0200

    add example, add git infos & keywords,  fix package.json

---
+++ example/example.js
@@ -0,0 +1,51 @@
+const fischcom = require('../index.js');
+
+// Set the baud rate and path
+let config = {
+  baudrate: 9600,
+  path: '/dev/ttyUSB0',
+  debug: false
+}
+
+fischcom.setSerialConfig(config)
+  .then(() => {
+    console.log('Serial configuration set');
+
+    // Read from the serial port
+    fischcom.readSerialData(config, onDataReceived);
+
+    // Write to the serial port
+    fischcom.writeSerialData(config, '1\r')
+      .then(() => {
+        console.log('Data written to serial port');
+      })
+      .catch(error => {
+        console.error('Error writing to serial port:', error);
+      });
+
+      // ask every second for data
+      setInterval(() => {
+        fischcom.writeSerialData(config, '1\r')
+        .then(() => {
+          console.log('Data written to serial port');
+        })
+        .catch(error => {
+          console.error('Error writing to serial port:', error);
+        });
+      }, 1000)
+      // -- ask every second for data --
+  })
+  .catch(error => {
+    console.error('Error setting serial configuration:', error);
+  });
+
+// Function to handle received data from the serial port
+function onDataReceived(data) {
+  console.log('Received data:', data);
+}
+
+// Function to gracefully stop the cat process when needed
+process.on('SIGINT', () => {
+  fischcom.stopCatProcess();
+  process.exit();
+});
---
+++ index.js
@@ -0,0 +1,94 @@
+const { spawn, exec } = require('child_process');
+
+let catProcess; // Store reference to the cat process
+
+function logger(data, debug=false) {
+  if(debug) {
+    console.log(data)
+  }
+}
+
+// Function to set the baud rate and path
+function setSerialConfig(config) {
+  return new Promise((resolve, reject) => {
+    let {baudrate, path, debug} = config
+    const command = `stty ${baudrate} -F ${path}`;
+    logger("--------", debug);
+    logger(`EXECUTE: ${command}`, debug);
+    logger("========", debug);
+    const process = exec(command, (error, stdout, stderr) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(stdout);
+      }
+    });
+    process.on('close', () => {
+      resolve();
+    });
+  });
+}
+
+// Function to read from the serial port
+function readSerialData(config, onData) {  
+  return new Promise((resolve, reject) => {
+    let {baudrate, path, debug} = config
+    const command = `cat ${path}`;
+    logger("--------", debug);
+    logger(`EXECUTE: ${command}`, debug);
+    logger("========", debug);
+    catProcess = spawn('sh', ['-c', command]);
+
+    catProcess.stdout.on('data', (data) => {
+      onData(data.toString());
+    });
+
+    catProcess.stderr.on('data', (data) => {
+      reject(data.toString());
+    });
+
+    catProcess.on('error', (error) => {
+      reject(error);
+    });
+
+    catProcess.on('close', () => {
+      resolve();
+    });
+  });
+}
+
+// Function to write to the serial port
+function writeSerialData(config, data) {
+  return new Promise((resolve, reject) => {
+    let {baudrate, path, debug} = config
+    const command = `echo -e "${data}" > ${path}`;
+    logger("--------", debug);
+    logger(`EXECUTE: ${command}`, debug);
+    logger("========", debug);
+    const process = exec(command, (error, stdout, stderr) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(stdout);
+      }
+    });
+    process.on('close', () => {
+      resolve();
+    });
+  });
+}
+
+// Function to stop the cat process and clean up resources
+function stopCatProcess() {
+  if (catProcess) {
+    catProcess.kill();
+    catProcess = null;
+  }
+}
+
+module.exports = {
+  setSerialConfig,
+  readSerialData,
+  writeSerialData,
+  stopCatProcess,
+};
